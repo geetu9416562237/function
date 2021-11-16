4a7e902775661d31d983186edc98db613be05104
Geetu Sharma <92966101+geetu9416562237@users.noreply.github.com>
2021-11-16 16:27:28 +0530

    Add files via upload

---
+++ recourse.py
@@ -0,0 +1,27 @@
+# a=open("2.txt","r")
+# a=a.read(43)
+# print(a)
+
+
+# f=open("2.text","w")
+# f=f.write("i am the best")
+# f=open("2.text","r")
+# f=f.read()
+# print(f)
+# i=0
+# count=0
+# while i<len(f):
+#     if f[i]!="":
+#         count+=1
+#     i+=1
+# print(count)
+
+
+
+Dict = {
+       "ball": 'green',
+       "Ball": 'red'
+     }
+print(Dict['ball'])
+print(Dict['Ball'])
+# print(Dict['bat'])
